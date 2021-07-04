5bed9cca8671e4a0b31d9bfe2ed8ce266a2340e6
Lippold Jackson <Jackson.Lippold@dssmith.com>
2021-07-04 20:59:03 +0100

    package test 1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='python_test',
+    version='0.0.1',
+    description='test package',
+    author='jackson',
+    url='https://github.com/Jackson-dss/python_test',
+    packages=['example_package'],
+    # install_requires=[
+    #     'unidecode',
+    #     'awesome-slugify'
+    # ]
+)
